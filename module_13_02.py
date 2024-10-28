630465ea6096351655a06b1824f66b5477f85f82
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-10-28 19:59:09 +0500

    Update module_13_02.py

--- module_13_02.py
+++ module_13_02.py
@@ -9,14 +9,12 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print("Привет! Я бот помогающий твоему здоровью.")
-    #await message.answer('Привет, я бот!')
+    print("Привет! Я бот помогающий твоему здоровью.")  
 
 
 @dp.message_handler()
 async def all_messages(message):
     print("Введите команду /start, чтобы начать общение.")
-    #await message.answer('Я бот, который поможет тебе!')
 
 
 if __name__ == '__main__':
